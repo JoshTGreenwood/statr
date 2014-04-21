51cbdb71d120ff7253cd9bb3ca3ebf58e88d7734
Josh Greenwood <joshua.t.greenwood@gmail.com>
2014-04-21 16:56:16 -0400

    deletes unnecessary require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require Chart
 //= require highcharts/highcharts                                                           
 //= require highcharts/highcharts-more                                                         
 
