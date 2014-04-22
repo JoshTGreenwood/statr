eeae861e0449968178a032b8105f4920eeddf07a
Josh Greenwood <joshua.t.greenwood@gmail.com>
2014-04-22 16:02:19 -0400

    added validations for game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,7 @@ class Game < ActiveRecord::Base
   has_and_belongs_to_many :players
   has_many :stats, dependent: :destroy
 
+  validates_presence_of :team, :opponent_name, :date_played
+
   after_create {|game| game.team.players.each{|player| Stat.create(game_id:game.id, player_id: player.id)}}
 end
